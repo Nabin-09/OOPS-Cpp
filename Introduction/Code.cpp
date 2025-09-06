af8017afd01fc7ba26c33d4f40f923f177d6064d
Nabin <sharmanabin.0910@gmail.com>
2025-09-06 12:57:46 +0530

    making a class

---
+++ Introduction/Code.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Student{
+public : 
+    string name;
+    int rno;
+    float cgpa;
+};
+int main(){
+
+
+    return 0;
+}
---
+++ Introduction/Notes.md
@@ -0,0 +1,5 @@
+# Classes and Objects
+
+Classes are basically user defined blueprint of data structures , whereas objects are <br> instances of classes. <br>
+
+
